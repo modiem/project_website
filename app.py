9b7a5244c6baf259c44f905d5950aa77e6f546e8
Mo Yang <mo_yang@foxmail.com>
2021-02-14 20:24:56 +0100

    page taxifare-prediction with map

--- app.py
+++ app.py
@@ -1,37 +1,84 @@
 import streamlit as st 
 import datetime
 import requests
+import pandas as pd
+from Packages.utils import geocoder_here, reverse_geocoder_here
 
-'''
-# Taxi Fare Prediction
+TAXIFARE_API = "https://predict-taxifare-iwuisdewea-ez.a.run.app/predict_fare"
 
-This front queries a [taxi fare model API]
-(https://predict-taxifare-iwuisdewea-ez.a.run.app/predict_fare)'''
+def main():
+    st.sidebar.header("Navigation")
+    page = st.sidebar.radio("Go to", ("Home", "Taxifare Prediction"))
+    if page == "Taxifare Prediction":
+        '''
+        # Taxi Fare Prediction
 
-key = '2012-10-06 12:10:20.0000001'
-pickup_date = st.date_input('pickup date', value=datetime.datetime.today())
-pickup_time = st.time_input('pickup time', value=datetime.datetime.now())
-pickup_datetime = f'{pickup_date} {pickup_time}UTC'
-pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+        This page queries a [taxi fare model API]
+        (https://predict-taxifare-iwuisdewea-ez.a.run.app/predict_fare)'''
 
-url = "https://predict-taxifare-iwuisdewea-ez.a.run.app/predict_fare"
 
-params = dict(
-    key=key,
-    pickup_datetime=pickup_datetime,
-    pickup_longitude=pickup_longitude,
-    pickup_latitude=pickup_latitude,
-    dropoff_longitude=dropoff_longitude,
-    dropoff_latitude=dropoff_latitude,
-    passenger_count=passenger_count
-)
 
-response = requests.get(url, params=params)
+        ## input pickup time
+        pickup_date = st.date_input('pickup date', value=datetime.datetime.today())
+        pickup_time = st.time_input('pickup time', value=datetime.datetime.now())
+        pickup_datetime = f'{pickup_date} {pickup_time}UTC'
 
-prediction = response.json()
-pred = prediction['prediction']
-pred
+        geo = st.radio("Select a Geo Format:", ("by address", "by coordinates"))
+        if geo == "by coordinates":
+            ## Get coordinates from user input and get address
+            #pickup place
+            pickup_lng = st.number_input('pickup longitude', value=40.7614327)
+            pickup_lat = st.number_input('pickup latitude', value=-73.9798156)
+            pickup_adress = reverse_geocoder_here((pickup_lng, pickup_lat))['adress']
+            pickup_longitude = reverse_geocoder_here((pickup_lng, pickup_lat))['lng']
+            pickup_latitude = reverse_geocoder_here((pickup_lng, pickup_lat))['lat']
+            st.info(f"The pickup address: {pickup_adress}")
+            #dropoff place
+            dropoff_lng = st.number_input('dropoff longitude', value=40.6413111)
+            dropoff_lat = st.number_input('dropoff latitude', value=-73.7803331)
+            dropoff_adress = reverse_geocoder_here((dropoff_lng, dropoff_lat))['adress']
+            dropoff_longitude = reverse_geocoder_here((dropoff_lng, dropoff_lat))['lng']
+            dropoff_latitude = reverse_geocoder_here((dropoff_lng, dropoff_lat))['lat'] 
+            st.info(f"The dropoff address: {dropoff_adress}")
+
+        if geo == "by address":
+            ## Get address from user input
+            pickup_adress = st.text_input("pickup address", "251 Church St, New York, NY 10013")
+            dropoff_adress = st.text_input("dropoff address", "434 6th Ave, New York, NY 10011")
+            ## Get coordinates
+            try:
+                pickup_latitude = geocoder_here(pickup_adress)['lat']
+                pickup_longitude = geocoder_here(pickup_adress)['lng']
+                dropoff_latitude = geocoder_here(dropoff_adress)['lat']
+                dropoff_longitude = geocoder_here(dropoff_adress)['lng']
+            except:
+                st.error("Couln't retrive coordinates from the adreess.")
+
+        ## Draw a map
+        map_data = pd.DataFrame({"lat":[pickup_latitude,dropoff_latitude], 
+                                 "lon":[pickup_longitude,dropoff_longitude]})
+        st.map(data=map_data)
+
+        passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+
+        ### Forat input
+        params = dict(
+            key='2012-10-06 12:10:20.0000001',
+            pickup_datetime=pickup_datetime,
+            pickup_longitude=pickup_longitude,
+            pickup_latitude=pickup_latitude,
+            dropoff_longitude=dropoff_longitude,
+            dropoff_latitude=dropoff_latitude,
+            passenger_count=passenger_count
+        )
+
+        response = requests.get(TAXIFARE_API, params=params)
+
+        prediction = response.json()
+        pred = prediction['prediction']
+        if st.button('Calculate Taxifare'):
+            st.write("Taxi Fare:", pred)
+
+if __name__ == "__main__":
+
+    main()
